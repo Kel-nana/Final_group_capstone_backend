e0032ba327619a2621b4e0bf278f5f9630189f3b
husush23 <husseinkadare40@gmail.com>
2023-09-29 11:52:36 +0300

    Validations added

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,10 @@
 class Appointment < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :doctor, dependent: :destroy
+
+  validates :user_id, presence: true
+  validates :doctor_id, presence: true
+  validates :appointment_date, presence: true
+  validates :appointment_time, presence: true
+  validates :location, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :doctors, dependent: :destroy
     has_many :appointments, dependent: :destroy
+
+    validates :name, presence: true
 end
