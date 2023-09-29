323fe569f8f37b7facd22afe2240b00326fc0ead
Nahid1911 <nahid.raihan@gmail.com>
2023-09-29 17:26:01 +0600

    Create Controllers for API endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :specializations
-  resources :doctors
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   namespace :api do
     namespace :v1 do
       resources :users
