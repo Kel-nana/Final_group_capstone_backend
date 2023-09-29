440065441436e51a498a80cd743d2c91e200a797
husush23 <husseinkadare40@gmail.com>
2023-09-30 00:35:45 +0800

    Indentation fixed

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,44 +1,44 @@
 class Api::V1::DoctorsController < ApplicationController
-    before_action :set_doctor, only: %i[show update destroy]
-  
-    def index
-      @doctors = Doctor.all
-      render json: @doctors
-    end
-  
-    def show
-      render json: @doctor
-    end
-  
-    def create
-      @doctor = Doctor.new(doctor_params)
-  
-      if @doctor.save
-        render json: @doctor, status: :created
-      else
-        render json: @doctor.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def update
-      if @doctor.update(doctor_params)
-        render json: @doctor
-      else
-        render json: @doctor.errors, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @doctor.destroy
-    end
-  
-    private
-  
-    def set_doctor
-      @doctor = Doctor.find(params[:id])
-    end
-  
-    def doctor_params
-      params.require(:doctor).permit(:doc_name, :education, :bio, :years_of_experience, :profile_pic)
+  before_action :set_doctor, only: %i[show update destroy]
+
+  def index
+    @doctors = Doctor.all
+    render json: @doctors
+  end
+
+  def show
+    render json: @doctor
+  end
+
+  def create
+    @doctor = Doctor.new(doctor_params)
+
+    if @doctor.save
+      render json: @doctor, status: :created
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
     end
   end
+
+  def update
+    if @doctor.update(doctor_params)
+      render json: @doctor
+    else
+      render json: @doctor.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @doctor.destroy
+  end
+
+  private
+
+  def set_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+
+  def doctor_params
+    params.require(:doctor).permit(:doc_name, :education, :bio, :years_of_experience, :profile_pic)
+  end
+end
