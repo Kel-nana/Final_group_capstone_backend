f29179fdeaab9ce1512de268d434dbf6a88c9ec0
husush23 <husseinkadare40@gmail.com>
2023-09-29 23:57:09 +0800

    Appointments controller updated

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,29 +1,25 @@
 class Api::V1::AppointmentsController < ApplicationController
   before_action :set_appointment, only: %i[show update destroy]
 
-  # GET /appointments
   def index
     @appointments = Appointment.all
     render json: @appointments
   end
 
-  # GET /appointments/1
   def show
     render json: @appointment
   end
 
-  # POST /appointments
   def create
     @appointment = Appointment.new(appointment_params)
 
     if @appointment.save
-      render json: @appointment, status: :created, location: @appointment
+      render json: @appointment, status: :created
     else
       render json: @appointment.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /appointments/1
   def update
     if @appointment.update(appointment_params)
       render json: @appointment
@@ -32,19 +28,16 @@ class Api::V1::AppointmentsController < ApplicationController
     end
   end
 
-  # DELETE /appointments/1
   def destroy
     @appointment.destroy
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_appointment
     @appointment = Appointment.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def appointment_params
     params.require(:appointment).permit(:user_id, :doctor_id, :appointment_date, :appointment_time, :location)
   end
