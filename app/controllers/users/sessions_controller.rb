b1f819ef1d779f131758f2938bdd5c3efd5d333a
husush23 <husseinkadare40@gmail.com>
2023-10-04 03:29:23 +0300

    devise controller added

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,27 +1,38 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  respond_to :json 
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def respond_with(resource, options={})
+    render json: {
+      status: {
+        code: 200,
+        message: "User signed in successfully",
+        data: current_user
+      },
+      status: :ok
+    }
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def respond_to_on_destroy
+    jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.credentials.fetch(:secret_key_base)).first
 
-  # protected
+    current_user = User.find(jwt_payload['sub'])
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    if current_user 
+      render json: {
+        status: 200,
+        message: "Signed out succesfully"
+      },
+      status: :ok
+    else
+      render json:{
+        status: 401,
+        message:"User has no active session"
+      },
+      status: :unauthorized
+    end
+  end
 end
