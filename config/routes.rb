f43147de3ac4c63bdbd632ad22bfd935f6edefd5
husush23 <husseinkadare40@gmail.com>
2023-10-05 21:09:31 +0300

    sign up/in API docs added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
+  # authenticate :user do
+    mount Rswag::Ui::Engine => '/api-docs'
+    mount Rswag::Api::Engine => '/api-docs'
+  # end
   devise_for :users, controllers:{
     sessions: "users/sessions",
     registrations: "users/registrations"
