64d9ac06e510a649cc57af7c137fce21f59d1a2d
kelvinirungu <kelvinn.irungu69@gmail.com>
2023-10-09 18:40:14 +0300

    Fix linter erros

--- spec/models/appointments_specs.rb
+++ spec/models/appointments_specs.rb
@@ -3,57 +3,55 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  describe "Validations" do
-
-    it "is not valid without a location" do
-        appointment = Appointment.new(
+  describe 'Validations' do
+    it 'is not valid without a location' do
+      appointment = Appointment.new(
         user_id: 1,
         doctor_id: 1,
-        appointment_date: "2015-10-10",
-        appointment_time: "12:00:00",
+        appointment_date: '2015-10-10',
+        appointment_time: '12:00:00'
       )
       expect(appointment).to_not be_valid
     end
 
-    it "is not valid without a appointment_time" do
-        appointment = Appointment.new(
+    it 'is not valid without a appointment_time' do
+      appointment = Appointment.new(
         user_id: 1,
         doctor_id: 1,
-        appointment_date: "2015-10-10",
-        location: "Nairobi",
+        appointment_date: '2015-10-10',
+        location: 'Nairobi'
       )
       expect(appointment).to_not be_valid
     end
 
-    it "is not valid without a appointment_date" do
-        appointment = Appointment.new(
+    it 'is not valid without a appointment_date' do
+      appointment = Appointment.new(
         user_id: 1,
         doctor_id: 1,
-        appointment_time: "12:00:00",
-        location: "Nairobi",
+        appointment_time: '12:00:00',
+        location: 'Nairobi'
       )
       expect(appointment).to_not be_valid
     end
 
-    it "is not valid without a user_id" do
-        appointment = Appointment.new(
+    it 'is not valid without a user_id' do
+      appointment = Appointment.new(
         doctor_id: 1,
-        appointment_time: "12:00:00",
-        appointment_date: "2015-10-10",
-        location: "Nairobi",
+        appointment_time: '12:00:00',
+        appointment_date: '2015-10-10',
+        location: 'Nairobi'
       )
       expect(appointment).to_not be_valid
     end
-
   end
 
-  describe "Associations" do
-    it "  belongs_to user" do
+  describe 'Associations' do
+    it '  belongs_to user' do
       expect(Appointment.reflect_on_association(:user).macro).to eq(:belongs_to)
     end
 
-    it "belongs_to doctors" do
-        expect(Appointment.reflect_on_association(:doctor).macro).to eq(:belongs_to)
-      end
+    it 'belongs_to doctors' do
+      expect(Appointment.reflect_on_association(:doctor).macro).to eq(:belongs_to)
+    end
   end
 end
--- spec/models/doctors_specs.rb
+++ spec/models/doctors_specs.rb
@@ -3,48 +3,48 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  describe "Validations" do
-    it "is valid with valid attributes" do
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
       doctor = Doctor.new(
-        doc_name: "Dr. John Smith",
-        education: "MD",
+        doc_name: 'Dr. John Smith',
+        education: 'MD',
         years_of_experience: 5
       )
       expect(doctor).to be_valid
     end
 
-    it "is not valid without a doc_name" do
+    it 'is not valid without a doc_name' do
       doctor = Doctor.new(
-        education: "MD",
+        education: 'MD',
         years_of_experience: 5
       )
       expect(doctor).to_not be_valid
     end
 
-    it "is not valid without education" do
+    it 'is not valid without education' do
       doctor = Doctor.new(
-        doc_name: "Dr. John Smith",
+        doc_name: 'Dr. John Smith',
         years_of_experience: 5
       )
       expect(doctor).to_not be_valid
     end
 
-    it "is not valid with negative years_of_experience" do
+    it 'is not valid with negative years_of_experience' do
       doctor = Doctor.new(
-        doc_name: "Dr. John Smith",
-        education: "MD",
+        doc_name: 'Dr. John Smith',
+        education: 'MD',
         years_of_experience: -1
       )
       expect(doctor).to_not be_valid
     end
   end
 
-  describe "Associations" do
-    it "has many appointments" do
+  describe 'Associations' do
+    it 'has many appointments' do
       expect(Doctor.reflect_on_association(:appointments).macro).to eq(:has_many)
     end
 
-    it "has and belongs to many specializations" do
+    it 'has and belongs to many specializations' do
       expect(Doctor.reflect_on_association(:specializations).macro).to eq(:has_and_belongs_to_many)
     end
   end
