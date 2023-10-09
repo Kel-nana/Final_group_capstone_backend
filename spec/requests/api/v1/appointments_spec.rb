b7b493d3603cabe9f68f6a057ab7ecc606a071ff
Junaid Syed <sjunaid626@gmail.com>
2023-10-09 17:58:41 +0530

    Create tests for `appointments_controller.rb`

---
+++ spec/requests/api/v1/appointments_spec.rb
@@ -0,0 +1,51 @@
+require 'rails_helper'
+
+RSpec.describe 'api/v1/appointments', type: :request do
+  describe 'GET/APPOINTMENTS' do
+    context 'without JWT' do
+      before do
+        get '/api/v1/appointments'
+      end
+
+      it 'returns unauthorized' do
+        expect(response.body).to include('You need to sign in or sign up before continuing.')
+      end
+    end
+
+    context 'with valid parameters' do
+      before do
+        create_appointment
+      end
+      
+      it 'get all appointments' do
+        get '/api/v1/appointments', headers: { Authorization: @authorization }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  def create_appointment
+    user = User.create(
+      name: 'max213',
+      email: 'max213@mail.com',
+      password: 'pass1234',
+      password_confirmation: 'pass1234',
+      jti: Faker::Alphanumeric.alpha(number: 10)
+    )
+    doctor = Doctor.create(
+      doc_name: 'john doe',
+      education: 'MBBS MD',
+      years_of_experience: 5,
+      bio: 'Neuro sergeon',
+      profile_pic: 'profilepic.jpg'
+    )
+    post '/users/sign_in', params:
+                      { user: {
+                        email: 'max213@mail.com',
+                        password: 'pass1234'
+                      } }
+    @authorization = response.headers['Authorization']
+    Appointment.create(  user_id: user.id, doctor_id: doctor.id, appointment_date: '2021-10-10', appointment_time: '2021-10-10 10:10:10', location: 'tester_333' )
+    Appointment.create(  user_id: user.id, doctor_id: doctor.id, appointment_date: '2022-08-04', appointment_time: '2021-10-10 10:10:10', location: 'tester_333' )
+  end
+end
