b0c04b4b9669314b28e202a014c4fa4a75f03476
Mohammad Nahid Raihan Sardar <nahid.raihan@gmail.com>
2023-10-11 21:40:39 +0600

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,27 +8,6 @@ specializations = [
 
 Specialization.create!(specializations)
 
-# db/seeds/doctors.rb
-# doctors = [
-#   { doc_name: 'Dr. John Doe', education: 'MD', bio: 'Cardiologist', years_of_experience: 10, location: 'New York' },
-#   { doc_name: 'Dr. Jane Smith', education: 'MD', bio: 'Dermatologist', years_of_experience: 8, location: 'Chicago' },
-#   { doc_name: 'Dr. Michael Johnson', education: 'MD', bio: 'Neurologist', years_of_experience: 12, location: 'Los Angeles' },
-#   # Add more doctors as needed
-# ]
-
-# Doctor.create!(doctors)
-
-# db/seeds/users.rb
-users = [
-  { name: 'Alice', email: 'alice1@example.com', password: 'password' },
-  { name: 'Bob', email: 'bob1@example.com', password: 'password' },
-  { name: 'Charlie', email: 'charlie1@example.com', password: 'password' },
-  # Add more users as needed
-]
-
-User.create!(users)
-
-
 doctors = [
   {
     doc_name: 'Antoni Shkraba',
